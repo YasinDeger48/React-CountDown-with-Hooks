766fea485b318c8a47a9a2c0ad01a87f5f979ed8
Yasin Değer <letsexploreb31@gmail.com>
2022-01-04 01:53:10 +0300

    countdown initial upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./style/main.css";
+
+function padTime(time) {
+  return time.toString().padStart(2, "0");
+}
 
 function App() {
+  const [title, setTitle] = useState("CountDown");
+  const [timeLeft, setTimeLeft] = useState(15);
+  const [isRunning, setisRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  const min = padTime(Math.floor(timeLeft / 60));
+  const sec = padTime(timeLeft - min * 60);
+
+  function startTimer() {
+    if (intervalRef.current !== null) return;
+    setTitle("Great");
+    setisRunning(true);
+
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => {
+        if (timeLeft >= 1) return timeLeft - 1;
+        return 0;
+      });
+    }, 1000);
+  }
+
+  function stopTimer() {
+    if (intervalRef.current == null) return;
+
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setisRunning(false);
+  }
+
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle("another round");
+    setTimeLeft(25 * 60);
+    setisRunning(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{title}</h2>
+
+      <div className="timer">
+        <span>{min}</span>
+        <span>:</span>
+        <span>{sec}</span>
+      </div>
+
+      <div className="buttons" style={{'textAlign':'center'}}>
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button onClick={stopTimer}>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
+      </div>
     </div>
   );
 }
